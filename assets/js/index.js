19128e62229007f9a31e4eecddd5675e61e8e1ce
Chino_HDP <Victort8012@gmail.com>
2021-10-09 12:19:47 -0300

    agregando eventos para los formularios de las transiciones

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,29 @@
 //! obtner datos desde el html a JavaScript
 const btnAutomata1 = document.querySelector('#btn-automata-1');
 const btnAutomata2 = document.querySelector('#btn-automata-2');
-let resp;
+const btnTransicionesA1 = document.querySelector('#transiciones1');
+const btnTransicionesA2 = document.querySelector('#transiciones2');
 
-//* Forma de agregar un evento en JavaScript
+let resp;
+let automata1, automata2;
+let transiciones;
+let transicionesA1 = [];
+let transicionesA2 = [];
+
+//?Evento formulario Automata1
 btnAutomata1.addEventListener('click', async event => {
   event.preventDefault(); //? Evitar que se recarge la pagina
 
   const alfabeto = document.querySelectorAll('#campo_alfabeto')[0]; //? Selecionar campos individuales en forma de array
   const estadoInicial = document.querySelectorAll('#campo_estadoInicial')[0];
   const estadoFinal = document.querySelectorAll('#campo_estadoFinal')[0];
-  const estadoBase = document.querySelectorAll('#estado_Inicial')[0];
-  const simbolo = document.querySelectorAll('#simbolo')[0];
-  const estadoLlegada = document.querySelectorAll('#estado_final')[0];
   const afd = document.querySelector('#AFD');
   const afnd = document.querySelector('#AFND');
 
-  const ejecutar = await formulario(
+  const ejecutar = await ejecutarFormularioAutomata(
     alfabeto,
     estadoInicial,
     estadoFinal,
-    estadoBase,
-    simbolo,
-    estadoLlegada,
     afd,
     afnd
   );
@@ -31,35 +32,36 @@ btnAutomata1.addEventListener('click', async event => {
     return;
   }
 
+  //? guardar los datos del automata1 como json
+  automata1 = {
+    alfabeto: alfabeto.value,
+    estadoInicial: estadoInicial.value,
+    estadoFinal: estadoFinal.value,
+    afd: afd.checked,
+    afnd: afnd.checked,
+  };
+
   alfabeto.value = '';
   estadoInicial.value = '';
   estadoFinal.value = '';
-  estadoBase.value = '';
-  simbolo.value = '';
-  estadoLlegada.value = '';
   afd.checked = false;
   afnd.checked = false;
 });
 
+//?Evento formulario Automata 2
 btnAutomata2.addEventListener('click', async event => {
   event.preventDefault(); //? Evitar que se recarge la pagina
 
   const alfabeto = document.querySelectorAll('#campo_alfabeto')[1]; //? Selecionar campos individuales en forma de array
   const estadoInicial = document.querySelectorAll('#campo_estadoInicial')[1];
   const estadoFinal = document.querySelectorAll('#campo_estadoFinal')[1];
-  const estadoBase = document.querySelectorAll('#estado_Inicial')[1];
-  const simbolo = document.querySelectorAll('#simbolo')[1];
-  const estadoLlegada = document.querySelectorAll('#estado_final')[1];
   const afd = document.querySelector('#AFD2');
   const afnd = document.querySelector('#AFND2');
 
-  const ejecutar = await formulario(
+  const ejecutar = await ejecutarFormularioAutomata(
     alfabeto,
     estadoInicial,
     estadoFinal,
-    estadoBase,
-    simbolo,
-    estadoLlegada,
     afd,
     afnd
   );
@@ -68,54 +70,78 @@ btnAutomata2.addEventListener('click', async event => {
     return;
   }
 
-  alfabeto.value = '';
-  estadoInicial.value = '';
-  estadoFinal.value = '';
-  estadoBase.value = '';
-  simbolo.value = '';
-  estadoLlegada.value = '';
-  afd.checked = false;
-  afnd.checked = false;
-});
-
-const formulario = async (
-  alfabeto,
-  estadoInicial,
-  estadoFinal,
-  estadoBase,
-  simbolo,
-  estadoLlegada,
-  afd,
-  afnd
-) => {
-  const validacionBasica = validaciones(
-    alfabeto.value,
-    estadoInicial.value,
-    estadoFinal.value,
-    estadoBase.value,
-    simbolo.value,
-    estadoLlegada.value,
-    afd.checked,
-    afnd.checked
-  );
-
-  if (!validacionBasica) {
-    return false;
-  }
-
-  //? guarda datos del usuario en forma de json para facilidad de transferencia de datos
-  const data = {
+  automata2 = {
     alfabeto: alfabeto.value,
     estadoInicial: estadoInicial.value,
     estadoFinal: estadoFinal.value,
-    estadoBase: estadoBase.value,
-    simbolo: simbolo.value,
-    estadoLlegada: estadoLlegada.value,
     afd: afd.checked,
     afnd: afnd.checked,
   };
 
-  peticionInfo(data);
+  alfabeto.value = '';
+  estadoInicial.value = '';
+  estadoFinal.value = '';
+  afd.checked = false;
+  afnd.checked = false;
+});
 
-  return true;
-};
+//?Enevnto transiciones Automata 1
+btnTransicionesA1.addEventListener('click', async event => {
+  event.preventDefault();
+
+  const estadoInicial = document.querySelectorAll('#estado_Inicial')[0];
+  const simbolo = document.querySelectorAll('#simbolo')[0];
+  const estadoFinal = document.querySelectorAll('#estado_final')[0];
+
+  const ejecutar = await ejecutarFormularioTransiciones(
+    estadoInicial.value,
+    simbolo.value,
+    estadoFinal.value
+  );
+
+  if (!ejecutar) {
+    return false;
+  }
+
+  transiciones = {
+    estadoInicial: estadoInicial.value,
+    estadoFinal: estadoFinal.value,
+    simbolo: simbolo.value,
+  };
+
+  transicionesA1.push(transiciones);
+
+  estadoInicial.value = '';
+  estadoFinal.value = '';
+  simbolo.value = '';
+});
+
+//?Enevnto transiciones Automata 2
+btnTransicionesA2.addEventListener('click', async event => {
+  event.preventDefault();
+
+  const estadoInicial = document.querySelectorAll('#estado_Inicial')[1];
+  const simbolo = document.querySelectorAll('#simbolo')[1];
+  const estadoFinal = document.querySelectorAll('#estado_final')[1];
+
+  const ejecutar = await ejecutarFormularioTransiciones(
+    estadoInicial.value,
+    simbolo.value,
+    estadoFinal.value
+  );
+
+  if (!ejecutar) {
+    return false;
+  }
+
+  transiciones = {
+    estadoInicial: estadoInicial.value,
+    estadoFinal: estadoFinal.value,
+    simbolo: simbolo.value,
+  };
+
+  transicionesA2.push(transiciones);
+  estadoInicial.value = '';
+  estadoFinal.value = '';
+  simbolo.value = '';
+});
